cbbc146662fe34261c4d4a3acf0990f06b7c6000
FauzanRamadhanPutra <100134490+FauzanRamadhanPutra@users.noreply.github.com>
2022-03-11 16:49:46 +0700

    Update 10.CekSeriPositif.c

--- Praktikum 3/10.CekSeriPositif.c
+++ Praktikum 3/10.CekSeriPositif.c
@@ -1,6 +1,6 @@
 /* Nama File : 10.CekSeriPositif.c*/
 /* Deskripsi : menampilkan total tahanan jika dirangkai seri*/
-/* nama Pembuat : 24060121140140-Fauzan Ramadhan Putra*/
+/* Pembuat : 24060121140140-Fauzan Ramadhan Putra*/
 /* Tanggal Pembuatan : Jumat, 11 Maret 2022*/
 
 #include <stdio.h>/*header file*/
